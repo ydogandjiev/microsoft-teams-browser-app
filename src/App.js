7ed33880475a864058867e58bc01a14b7f27a034
Yuri Dogandjiev <yuri.dogandjiev@live.com>
2019-07-22 16:02:36 -0700

    Implement browser

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Button, Input } from "@stardust-ui/react";
+import Iframe from "react-iframe";
+import { withRouter } from "react-router-dom";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    newAddress: "",
+    address: "https://www.bing.com"
+  };
+
+  onBack = event => {
+    this.props.history.goBack();
+  };
+
+  onForward = event => {
+    this.props.history.goForward();
+  };
+
+  onChange = event => {
+    this.setState({ newAddress: event.target.value });
+  };
+
+  onKeyDown = event => {
+    if (event.key === "Enter") {
+      this.setState({
+        address: this.state.newAddress
+      });
+    }
+  };
+
+  onClick = event => {
+    this.setState({
+      address: this.state.newAddress
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <div className="Header">
+          <Button
+            className="Button"
+            icon={{
+              name: "chevron-down",
+              rotate: 90
+            }}
+            iconOnly
+            onClick={this.onBack}
+          />
+          <Button
+            className="Button"
+            icon={{
+              name: "chevron-down",
+              rotate: -90
+            }}
+            iconOnly
+            onClick={this.onForward}
+          />
+          <Input
+            fluid
+            placeholder="Type a URL..."
+            onChange={this.onChange}
+            onKeyDown={this.onKeyDown}
+          />
+          <Button
+            className="Button"
+            icon="play"
+            iconOnly
+            onClick={this.onClick}
+          />
+        </div>
+        <Iframe src={this.state.address} width="100%" height="100%" />
+      </div>
+    );
+  }
 }
 
-export default App;
+export default withRouter(App);

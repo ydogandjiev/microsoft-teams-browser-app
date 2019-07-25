c26aefefcf8e06e05b7ca7bf5e38d17e7271d046
Yuri Dogandjiev <yuri.dogandjiev@live.com>
2019-07-24 23:14:06 -0700

    Fix scrolling behavior

--- src/App.js
+++ src/App.js
@@ -71,7 +71,9 @@ class App extends Component {
             onClick={this.onClick}
           />
         </div>
-        <Iframe src={this.state.address} width="100%" height="100%" />
+        <div class="Scroller">
+          <Iframe src={this.state.address} width="100%" height="100%" />
+        </div>
       </div>
     );
   }
